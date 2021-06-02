9943a5380a74882f190cb884a8fc1eeee8bac3b6
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-06-02 05:13:11 -0500

    basic crud functionality and database implemented

---
+++ FlightDocs/Models/AircraftContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightDocs.Models
+{
+    public class AircraftContext : DbContext
+    {
+        public AircraftContext(DbContextOptions<AircraftContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AircraftItem> AircraftItems { get; set; }
+    }
+}
---
+++ FlightDocs/Models/AircraftItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightDocs.Models
+{
+    public class AircraftItem
+    {
+        public int Id { get; set; }
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime LogDate { get; set; }
+        public int? LogHours { get; set; } // Need to Figure out how to set so if can be null
+        public int? IntervalMonths { get; set; } // Need to Figure out how to set so if can be null
+        public int? IntervalHours { get; set; } // Need to Figure out how to set so if can be null
+    }
+}
