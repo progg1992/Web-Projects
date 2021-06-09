04ad4f077f246f521d45416f817c349f84fdca06
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-06-09 02:44:59 -0500

    initial commit

---
+++ ASP-NET/Flight Docs/Filght Docs/Controllers/AircraftController.cs
@@ -0,0 +1,26 @@
+﻿using Filght_Docs.IService;
+using Filght_Docs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Filght_Docs.Controllers
+{
+    
+    [ApiController]
+    public class AircraftController : ControllerBase 
+    {
+        
+        // POST <AirplaneController>
+        [HttpPost("aircraft/{aircraftId}/duelist")]
+        public AircraftResponseDTO Post(int aircraftId, [FromBody] List<TaskInputDTO> tasks)
+        {
+            
+            
+            return new AircraftResponseDTO() { AircraftId = aircraftId};
+        }
+    }
+}
