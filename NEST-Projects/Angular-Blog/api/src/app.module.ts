8b6ba9eec4e24058be6e0ca1c7b439a5e752154b
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-06-18 17:44:25 -0500

    created user module

--- NEST-Projects/Angular-Blog/api/src/app.module.ts
+++ NEST-Projects/Angular-Blog/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { AppService } from './app.service';
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
