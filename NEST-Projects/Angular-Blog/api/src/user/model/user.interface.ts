fe7ec5385b4bf07dadbbd0ffab62453b85b1db3f
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-06-19 18:40:03 -0500

    feature/jwt

--- NEST-Projects/Angular-Blog/api/src/user/model/user.interface.ts
+++ NEST-Projects/Angular-Blog/api/src/user/model/user.interface.ts
@@ -1,6 +1,14 @@
 /* eslint-disable prettier/prettier */
 export interface User {
+<<<<<<< HEAD
+	id?: number;
+	name?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+=======
   id?: number;
   name?: string;
   username?: string;
+>>>>>>> cb660b093820e831ec0c8d15c6816f25b3cca0b1
 }
--- NEST-Projects/Angular-Blog/api/src/user/user.module.ts
+++ NEST-Projects/Angular-Blog/api/src/user/user.module.ts
@@ -3,10 +3,19 @@ import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './model/user.entity';
+<<<<<<< HEAD
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
+=======
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity])
+>>>>>>> cb660b093820e831ec0c8d15c6816f25b3cca0b1
   ],
   providers: [UserService],
   controllers: [UserController]
