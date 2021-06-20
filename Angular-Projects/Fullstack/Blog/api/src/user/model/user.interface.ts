09a2548b7754a101ebc6f6c8764a856536e41e6c
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-06-20 14:13:41 -0500

    Role Based Authentication

---
+++ Angular-Projects/Fullstack/Blog/api/src/user/model/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id?: number;
+    name?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    role?: UserRole;
+}
+
+export enum UserRole {
+    ADMIN = 'admin',
+    CHIEFEDITOR = 'chiefeditor',
+    EDITOR = 'editor',
+}
---
+++ Angular-Projects/Fullstack/Blog/api/src/user/service/user.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../model/user.entity';
+import { Repository } from 'typeorm';
+import { User } from '../model/user.interface';
+import { Observable, from, throwError } from 'rxjs';
+import { switchMap, map, catchError} from 'rxjs/operators';
+import { AuthService } from '../../auth/service/auth.service';
+
+@Injectable()
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+        private authService: AuthService
+    ) {}
+
+    create(user: User): Observable<User> {
+        return this.authService.hashPassword(user.password).pipe(
+            switchMap((passwordHash: string) => {
+                const newUser = new UserEntity();
+                newUser.name = user.name;
+                newUser.username = user.username;
+                newUser.email = user.email;
+                newUser.password = passwordHash;
+                newUser.role = user.role;
+
+                return from(this.userRepository.save(newUser)).pipe(
+                    map((user: User) => {
+                        const {password, ...result} = user;
+                        return result;
+                    }),
+                    catchError(err => throwError(err))
+                )
+            })
+        )
+    }
+
+    findOne(id: number): Observable<User> {
+        return from(this.userRepository.findOne({id})).pipe(
+            map((user: User) => {
+                const {password, ...result} = user;
+                return result;
+            } )
+        )
+    }
+
+    findAll(): Observable<User[]> {
+        return from(this.userRepository.find()).pipe(
+            map((users: User[]) => {
+                users.forEach(function (v) {delete v.password});
+                return users;
+            })
+        );
+    }
+
+    deleteOne(id: number): Observable<any> {
+        return from(this.userRepository.delete(id));
+    }
+
+    updateOne(id: number, user: User): Observable<any> {
+        delete user.email;
+        delete user.password;
+
+        return from(this.userRepository.update(id, user));
+    }
+
+    updateRoleOfUser(id: number, user: User): Observable<any> {
+        return from(this.userRepository.update(id, user));
+    }
+
+    login(user: User): Observable<string> {
+        return this.validateUser(user.email, user.password).pipe(
+            switchMap((user: User) => {
+                if(user) {
+                    return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+                } else {
+                    return 'Wrong Credentials';
+                }
+            })
+        )
+    }
+
+    validateUser(email: string, password: string): Observable<User> {
+        return this.findByMail(email).pipe(
+            switchMap((user: User) => this.authService.comparePasswords(password, user.password).pipe(
+                map((match: boolean) => {
+                    if(match) {
+                        const {password, ...result} = user;
+                        return result;
+                    } else {
+                        throw Error;
+                    }
+                })
+            ))
+        )
+
+    }
+
+    findByMail(email: string): Observable<User> {
+        return from(this.userRepository.findOne({email}));
+    }
+}
