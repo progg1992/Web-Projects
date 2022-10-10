d246143d86afa887ffb150c3c226eb22ad8c71c7
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2022-10-09 20:12:28 -0500

    Feature/basic auth (#3)
    * User Feature
    
    * user api
    
    * updated directory structure and implemented basic and advanced authentication

---
+++ Angular Projects/Blog Application/api/src/auth/decorators/roles.decorator.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import { SetMetadata } from "@nestjs/common";
+
+export const hasRoles = (...hasRoles: string[]) => SetMetadata('roles', hasRoles);
---
+++ Angular Projects/Blog Application/api/src/auth/guards/jwt.strategy.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { 'user': payload.user };
+  }
+}
