1696ead053d17fd1318bc9332821e2eb973b2a76
progg1992 <pierce.rogg@mymail.nwtc.edu>
2022-10-21 19:31:32 -0500

    added basic test for calc service

---
+++ Angular Projects/testing-project/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,21 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('CalculatorService', () => {
+
+    it('should add two numbers', () => {
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+
+    });
+
+    it('should subtract two numbers', () => {
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, 'unexpected subtraction result');
+
+    })
+})
