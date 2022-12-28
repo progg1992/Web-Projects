af53c465363b371698ed30e4b698f5faa02ff268
progg1992 <pierce.rogg@mymail.nwtc.edu>
2022-12-27 20:37:47 -0600

    finished router setup

--- Vue-Projects/firebase-vue-subscriptions/src/router/index.js
+++ Vue-Projects/firebase-vue-subscriptions/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AccountView from '../views/Account.vue'
+import SignInView from '../views/Signin.vue'
+import { getCurrentUser } from '../firebase'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,20 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/account',
+    name: 'account',
+    componet: AccountView,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: SignInView,
+    meta: {
+      guestOnly: true
+    }
   }
 ]
 
@@ -24,6 +35,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isGuestOnly = to.matched.some(record => record.meta.guestOnly)
+
+  const currentUser = await getCurrentUser()
+
+  if(requiresAuth && !currentUser) {
+    next('/signin')
+  } else if (isGuestOnly && currentUser) {
+    next('/account')
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ Vue-Projects/firebase-vue-subscriptions/src/views/Signin.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        Sign In Page
+    </div>
+</template>
