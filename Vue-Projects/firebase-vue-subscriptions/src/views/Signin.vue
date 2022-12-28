f832df203215f74020a406750f162572480ae6e7
progg1992 <pierce.rogg@mymail.nwtc.edu>
2022-12-27 20:41:16 -0600

    added google oauth

--- Vue-Projects/firebase-vue-subscriptions/src/views/Signin.vue
+++ Vue-Projects/firebase-vue-subscriptions/src/views/Signin.vue
@@ -1,6 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-        Sign In Page
+        Sign in page! <br />
+        <button @click="signIn">Sign in with Google</button>
     </div>
 </template>
+
+<script>
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { firebaseAuth } from '../firebase'
+
+export default {
+    methods: {
+        signIn() {
+            const provider = new GoogleAuthProvider()
+            return signInWithPopup(firebaseAuth, provider)
+                .then(() => this.$router.replace({ name: 'Account' }))
+        }
+    }
+}
+</script>
