dee3700e9302aaeba6719576bfb52c7241819e6b
progg1992 <pierce.rogg@mymail.nwtc.edu>
2022-12-27 19:29:27 -0600

    added firebase and router

--- Vue-Projects/vue-emailjs-demo/src/App.vue
+++ Vue-Projects/vue-emailjs-demo/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <contact-form />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactForm from './components/ContactForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContactForm
   }
 }
 </script>
